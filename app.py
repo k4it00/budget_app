f20622e8e31e80cd6791d3ba2d77e5c6c116bb70
k4it00 <zhendolion2018@gmail.com>
2024-12-07 15:25:54 +0100

    update base.css and app.py

--- app.py
+++ app.py
@@ -1935,5 +1935,5 @@ if __name__ == '__main__':
     with app.app_context():
         init_db()  # Initialize database and add default categories
         db.create_all()
-    
+    #app.run(debug=True)
 
