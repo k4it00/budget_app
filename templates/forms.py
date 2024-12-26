e8bf4a73a7d49419f0412e1840041caa3e50b6ac
k4it00 <zhendolion2014@gmail.com>
2024-12-27 00:36:10 +0100

    Clean up repository and apply .gitignore rules

--- templates/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, FloatField, SelectField, DateField, TextAreaField, BooleanField
-from wtforms.validators import DataRequired, Optional, Length, NumberRange
-from datetime import datetime
-
-class TransactionForm(FlaskForm):
-    
--- templates/l.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key-here'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///budget.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    BABEL_DEFAULT_LOCALE = 'en'
-    LANGUAGES = ['en', 'hu']
