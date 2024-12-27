bffbbe8111fc5ab036af234187b6e662723101cc
k4it00 <zhendolion2014@gmail.com>
2024-12-27 01:32:01 +0100

    comment app.run

--- config.py
+++ config.py
@@ -5,8 +5,9 @@ import secrets
 load_dotenv()
 class Config:
     SECRET_KEY = secrets.token_urlsafe(16)
-    client_id = os.getenv('GOOGLE_CLIENT_ID')
-    client_secret = os.getenv('GOOGLE_CLIENT_SECRET')
+    GOOGLE_CLIENT_ID = os.getenv('GOOGLE_CLIENT_ID')
+    GOOGLE_CLIENT_SECRET  = os.getenv('GOOGLE_CLIENT_SECRET')
     CACHE_TYPE = 'redis'
     CACHE_REDIS_URL = 'redis://localhost:6379/0'
     CDN_DOMAIN = os.environ.get('CDN_DOMAIN') or 'https://your-cdn-domain.com'
+
