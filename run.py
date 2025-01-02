dcc60d148962b52d9911f6c8812bd122eef729af
k4it00git config user.email zhendolion2018@gmail.comgit config user.name k4it00 <zhendolion2018@gmail.com>
2025-01-02 15:38:08 +0100

    logos

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ if __name__ == '__main__':
     with app.app_context():
         init_db()  # Initialize database and add default categories
         db.create_all()
-    #app.run(debug=True)
+    app.run(debug=True)
